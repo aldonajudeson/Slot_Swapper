2caf5ec98e8d910fb9623717242641c050a58647
aldonajudeson <aldonajudeson@gmail.com>
2025-10-31 01:41:57 +0530

    new commit

---
+++ backend/models/eventModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["BUSY", "SWAPPABLE", "SWAP_PENDING"],
+      default: "BUSY",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Event", eventSchema);
